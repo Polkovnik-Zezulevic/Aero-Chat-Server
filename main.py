c3a84cc48280f6eccbd0d49242359e45f06bd745
Полковник Зезюлевич <zzezulevic@gmail.com>
2026-07-28 10:31:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -10,11 +12,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-messages = []
+FILE = "messages.json"
+
+# Загружаем сообщения из файла при запуске
+if os.path.exists(FILE):
+    with open(FILE, "r", encoding="utf-8") as f:
+        messages = json.load(f)
+else:
+    messages = []
+
+# Функция сохранения
+def save_messages():
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(messages, f, ensure_ascii=False, indent=2)
 
 @app.post("/send")
 def send(data: dict):
     messages.append({"name": data["name"], "msg": data["msg"]})
+    save_messages()  # сохраняем каждый раз
     return {"ok": True}
 
 @app.get("/messages")
